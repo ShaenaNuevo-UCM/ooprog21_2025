4b22f34ec6ccefd9da4c4d9e7cd661e09a1bfd5f
ShaenaNuevo-UCM <shaenanuevo@gmail.com>
2025-09-16 08:52:14 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,16 @@
+/* This is the Comments Class 
+   This is a multi-line comment
+*/
+public class  Comments {
+   public static void main(String[] args) {
+      // This is a single-line comment
+      System.out.println("Hello, this program demonstrates comments.");
+      
+      /* 
+       *This is a documentation comment
+       *it's ussed for generating Javadoc
+      */
+      System.out.println("Java supports multi-line comments too.");
+      
+   }
+}
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+   public static void main(String[] args){
+      System.out.println("Country roads, take me home");
+      System.out.println("To the place I belong");
+      System.out.println("West Virginia, mountain mama");
+      System.out.println("Take me home, country roads");
+   }
+}

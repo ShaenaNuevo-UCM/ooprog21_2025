224ed1180b170721183ab76e31cf6ad2d442697c
ShaenaNuevo-UCM <shaenanuevo@gmail.com>
2025-09-23 08:40:42 +0800

    Update DebugBox.java

--- Chapter_4/DebugBox.java
+++ Chapter_4/DebugBox.java
@@ -1,1 +1,38 @@
+public class DebugBox
+{
+   private int width;
+   private int length;
+   private int height;
+   public DebugBox()
+   {
+      length = 1;
+      width = 1;
+      height = 1;
+   }
+   public DebugBox(int width, int length, int height)
+   {
+      this.width = width;
+      this.length = width;
+      this.height = width;
+   }
+   public void showData()
+   {
+      System.out.println("Width : "  + width 
+                       + "\nLength: "  + length 
+                       + "\nHeight: "  + height);
+   }
+   public double  getVolume(int width, int length, int height)
+   { 
+      double vol = length * width * height;
+      return vol;
+   }
+   
+
+   public static void main (String[] args){
+      DebugBox defaultBox = new DebugBox();
+      System.out.println("Default Box:");
+      defaultBox.showData();
+      System.out.println("Volume: " + defaultBox.getVolume(1, 1, 1));
+   } 
+}
 

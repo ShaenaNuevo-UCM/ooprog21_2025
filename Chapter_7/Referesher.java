0feed0183defc8d142ee9f321a9c5cc92949cea9
ShaenaNuevo-UCM <shaenanuevo@gmail.com>
2025-11-18 18:50:03 +0800

    Update Referesher.java

--- Chapter_7/Referesher.java
+++ Chapter_7/Referesher.java
@@ -1,22 +1,25 @@
-//*NOT FINISHED
-public class Referesher 
-{
-   public static void main(String args[])
-   {
-        int average;
-        
-   }
-   
-   public static void arrayInput()
-   {
-      String [] responseTimes = {100, 200, 150, 300};
-   }
-   
-   public static void calculations()
-   {
-     for(String i; i < arr.length ; i++)
-     {
-         average = responseTimes[0] / 
-     }
-   }
-}*\\
+public class Referesher {
+    public static void main(String[] args) {
+
+        int[] responseTimes = {100, 200, 150, 300};
+        int count = 0;
+
+        int sum = responseTimes[0]; 
+        int elements = 1;         
+    
+        for (int i = 1; i < responseTimes.length; i++) {
+
+            double average = (double) sum / elements;
+
+            if (responseTimes[i] > average) {
+                count++;
+            }
+
+            
+            sum += responseTimes[i];
+            elements++;
+        }
+
+        System.out.println(count);
+    }
+}

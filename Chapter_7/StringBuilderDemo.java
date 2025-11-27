45ebe9c71469572c2b92e2d5d65d5433c5bc11ee
ShaenaNuevo-UCM <shaenanuevo@gmail.com>
2025-11-27 08:03:30 +0800

    Update StringBuilderDemo.java

--- Chapter_7/StringBuilderDemo.java
+++ Chapter_7/StringBuilderDemo.java
@@ -1,1 +1,33 @@
-//NOT FINISHED
+import java.util.*;
+public class StringBuilderDemo 
+{
+   public static void main(String[] args)
+   {
+   
+      Scanner input = new Scanner(System.in);
+      
+      System.out.print("Enter a name:" );
+      String name = input.nextLine();
+      
+      System.out.print("Enter an address: ");
+      String address = input.nextLine();
+      
+      StringBuilder nameString = new StringBuilder(name);
+      StringBuilder addressString = new StringBuilder(address);
+      
+      System.out.println("\n(StringBuilder 1) nameString: " + nameString);
+      System.out.println("Capacity of nameString is: " + nameString.capacity());
+      
+      System.out.println("\n(StringBuilder 2) addressString: " + addressString);
+      System.out.println("Capacity of addressString is: " + addressString.capacity());
+      
+      nameString.setLength(20);
+      addressString.setLength(20);
+      
+      System.out.println("(Length set to 20 for StringBuilder 1) The name is " + nameString);
+      System.out.println("(Length set to 20 for StringBuilder 2) The address is " + addressString);
+      
+      input.close();
+
+   }
+}
